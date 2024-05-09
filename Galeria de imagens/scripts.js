a18342ec319ca5c7b097d28b6f6c55380932b2da
ruanbauerr <ruanbauer4@gmail.com>
2024-05-09 07:39:28 -0300

    scripts

---
+++ Galeria de imagens/scripts.js
@@ -0,0 +1,23 @@
+//o vento DOM é acionado quando o HTML foi completamente carregado
+
+document.addEventListener("DOMContentLoaded", function () {
+  alert("POMBO YAG");
+
+  //tipos de dados
+  //string: sequencia de caracter
+  //Number: numeros inteiros ou pontos flutuantes (10.4)
+  //boolean: retorna dois valores, true ou false
+  //object: estrutura de dados que pode armazenar multiplos valores
+  //NULL: quando esta vazio
+  //array: objeto especial ordenado
+  //function: bloco de codigo reutilizavel
+
+  //CONST: é usado para declarar uma variavel cujo valor é permanente
+  const test = 50;
+
+  //LET: é usado para declarar variaveis que os valores podem ser retribuidos
+
+  let C = 0;
+  C = 10;
+  console.log(C);
+});
